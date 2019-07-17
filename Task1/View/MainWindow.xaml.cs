00b03fa463c44a06a9f8344a9eacb318f2fdd497
tpa.admin <tpa.admin@DESKTOP-HJHOPHP>
2019-07-17 17:41:28 +0300

    Искправление кодировки в MainWindow.xaml.cs

--- Task1/View/MainWindow.xaml.cs
+++ Task1/View/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Windows;
-using System.IO;
-using System.Net;
-using Task1.ViewModel;
+п»їusing System.Windows;
 
 namespace Task1.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Integration logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        //создает главное окно
+        //СЃРѕР·РґР°РµС‚ РіР»Р°РІРЅРѕРµ РѕРєРЅРѕ
         public MainWindow()
         {
             InitializeComponent();
